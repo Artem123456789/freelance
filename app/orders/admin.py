16a10fc5dd685f45300b5abee55ab37cf053b299
artem <zhiruyev_a@bankrkb.kz>
2023-05-11 15:07:16 +0600

    orders admin

--- app/orders/admin.py
+++ app/orders/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Tag,
+    Category,
+    OrderExecution,
+    OrderExecutionCustomerInfo,
+    OrderExecutionEmployeeInfo,
+    Order,
+    OrderTags,
+    OrderCategory,
+    OrderResponse,
+)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(OrderExecutionEmployeeInfo)
+class OrderExecutionEmployeeInfoAdmin(admin.ModelAdmin):
+    search_fields = ['user__username']
+
+
+@admin.register(OrderExecutionCustomerInfo)
+class OrderExecutionCustomerInfoAdmin(admin.ModelAdmin):
+    search_fields = ['user__username']
+
+
+@admin.register(OrderExecution)
+class OrderExecutionAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'user__username']
+
+
+@admin.register(OrderTags)
+class OrderTagsAdmin(admin.ModelAdmin):
+    search_fields = ['order__title']
+
+
+@admin.register(OrderCategory)
+class OrderCategoryAdmin(admin.ModelAdmin):
+    search_fields = ['order__title']
+
+
+@admin.register(OrderResponse)
+class OrderResponseAdmin(admin.ModelAdmin):
+    search_fields = ['user__username']
