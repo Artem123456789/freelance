3648e69744994cf1a2b622a91fda213399bade30
artem <zhiruyev_a@bankrkb.kz>
2023-05-13 12:28:05 +0600

    init

--- app/_auth/handlers/auth_handlers.py
+++ app/_auth/handlers/auth_handlers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 
+from rest_framework.exceptions import PermissionDenied
+
 from app._auth.entities.auth_entities import RegisterInputEntity
 
 User = get_user_model()
@@ -8,6 +10,9 @@ User = get_user_model()
 class AuthHandler:
 
     def register(self, input_entity: RegisterInputEntity) -> User:
+        if User.objects.filter(username=input_entity.username).exists():
+            raise PermissionDenied("User with this username exists")
+
         user = User(username=input_entity.username)
         user.set_password(input_entity.password)
         user.save()
