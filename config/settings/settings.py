ad01c53829f84c64f27d2aad6002014cfbe5d209
artem <artem@mai.ru>
2023-06-11 22:17:21 +0600

    strip added

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -24,7 +24,7 @@ def get_secret(key, default):
     value = os.getenv(key, default)
     if os.path.isfile(value):
         with open(value) as f:
-            return f.read()
+            return f.read().strip()
     return value
 
 
